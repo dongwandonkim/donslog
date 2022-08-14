432ff5f2b36f0b17d34bd96563e530d4b2860473
Dongwan Kim <dongwan.don.kim@gmail.com>
2022-08-13 22:15:45 -0700

    comment schema

---
+++ backend/models/Comment.js
@@ -0,0 +1,18 @@
+const {
+  Schema,
+  model,
+  Types: { ObjectId },
+} = require('mongoose');
+
+const commentSchema = new Schema(
+  {
+    content: { type: String, required: true },
+    user: { type: ObjectId, required: true, ref: 'User' },
+    blog: { types: ObjectId, required: true, ref: 'Blog' },
+  },
+  { timestamps: true }
+);
+
+const Comment = model('comment', commentSchema);
+
+module.exports = Comment;
